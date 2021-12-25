611804ef0d6acbc2a12c9c80f5f3bf00a3554aaf
muruj <muruj1419@gmail.com>
2021-12-25 16:22:09 +0300

    add element

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ let time =0;
 let timerId =0;
 let timerOut= true;
 
+// element
+const start = document.querySelector("#start");
+const reset = document.querySelector("#reset");
+const timer = document.querySelector("#time-display");
+
+const initClock = () => {};
+const stopClock = () => {};
+
+start.addEventListener("click", function() {});
+
+reset.addEventListener("click", function() {});
