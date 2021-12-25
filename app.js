8a51da54c210e8f0868008c4967881d914da7659
muruj <muruj1419@gmail.com>
2021-12-25 16:52:33 +0300

    add timerCount

--- app.js
+++ app.js
@@ -8,9 +8,30 @@ const start = document.querySelector("#start");
 const reset = document.querySelector("#reset");
 const timer = document.querySelector("#time-display");
 
-const initClock = () => {};
+const initClock = () => {
+timerOut = false;
+timerId = setInterval(()=> {
+time++;
+timerCount();
+
+},1000);
+};
+
+const timerCount = {} => {
+const min = Math.floor(time/60);
+const sec = time %60;
+
+if (sec < 10) {
+timer.innerHTML =  `${min}:0${sec}`;
+    
+} else {
+timer.innerHTML =  `${min}:${sec}`;
+}
+};
 const stopClock = () => {};
 
-start.addEventListener("click", function() {});
+start.addEventListener("click", function() {
+initClock ();  
+});
 
 reset.addEventListener("click", function() {});
